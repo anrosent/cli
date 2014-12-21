022fa021504da7708ee95a708ab1262da285cc11
anrosent <Anson_Rosenthal@brown.edu>
2014-12-21 02:22:36 -0500

    added functionality to join CLIs by prefixing the commands of the joined CLI by a certain character sequence

--- cli.py
+++ cli.py
@@ -15,6 +15,7 @@ class CLI(object):
         self.aliases = {}
         self.add_func(self.print_help, ['h', 'help'])
         self.add_func(self.quit,       ['q', 'quit'])
+        self.builtin_cmds = frozenset(('h', 'help', 'q', 'quit'))
 
     def add_func(self, callback, names, *args):
         """Adds a command to the CLI - specify args as you would to
@@ -32,6 +33,26 @@ class CLI(object):
             self.cmds[names] = (callback, parser)
         else:
             raise TypeError("Command must be specified by str name or list of names")
+
+    def add_cli(self, prefix, other_cli, sep='-'):
+        """Adds the functionality of the other CLI to this one, where all
+        commands to the other CLI are prefixed by the given prefix plus a hyphen.
+
+        e.g. To execute command `greet anson 5` on other cli given prefix cli2, you
+        can execute the following with this CLI:
+            cli2-greet anson 5
+
+        The hyphen joining the prefix and command name can be changed using the `sep`
+        keyword argument.
+        """
+        for cmd, (fun, parser) in other_cli.cmds.items():
+            if cmd not in self.builtin_cmds:
+                new_cmd = sep.join((prefix, cmd))
+                if new_cmd not in self.cmds:
+                    self.cmds[new_cmd] = (fun, parser)
+                else:
+                    raise ValueError("Command %s already defined. Override not allowed.")
+
     
     def _dispatch(self, cmd, args):
         """Attempt to run the given command with the given arguments
