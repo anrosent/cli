fcd06bb15c8f685957e13b14b3902bbe89c8085c
anrosent <Anson_Rosenthal@brown.edu>
2014-12-20 19:08:10 -0500

    collapse aliases into one line in help

--- cli.py
+++ cli.py
@@ -6,13 +6,15 @@ validation and parsing logic to the `argparse` Standard Library module.
 import sys
 import argparse
 
-
 class CLI(object):
     """The Command Line Interface runner        
     """
 
     def __init__(self):
-        self.cmds = {} 
+        self.cmds = {}
+        self.aliases = {}
+        self.add_func(self.print_help, ['h', 'help'])
+        self.add_func(self.quit,       ['q', 'quit'])
 
     def add_func(self, callback, names, *args):
         """Adds a command to the CLI - specify args as you would to
@@ -22,6 +24,7 @@ class CLI(object):
         if isinstance(names, list):
             for name in names:
                 self.add_func(callback, name, *args)
+                self.aliases[name] = names
         elif isinstance(names, str):
             parser = argparse.ArgumentParser(prog=names)
             for cmd, spec in args:
@@ -56,6 +59,27 @@ class CLI(object):
     def _invalid_cmd(self, command=''):
         print("Invalid Command: %s" % command)
 
+    def print_help(self):
+        """Prints usage of all registered commands, collapsing aliases
+        into one record
+        """
+        seen_aliases = set()
+        print('-'*80)
+        for cmd, (fun, parser) in self.cmds.items():
+            if cmd != 'help':
+                if cmd not in seen_aliases:
+                    if cmd in self.aliases:
+                        seen_aliases.update(self.aliases[cmd])
+                        cmd = '/'.join(self.aliases[cmd])
+                    print(cmd)
+                    parser.print_usage()
+                    print('-'*80)
+
+    def quit(self):
+        """Quits the REPL
+        """
+        print('Quitting.')
+        sys.exit(0)
 
     def run(self, instream=sys.stdin):
         """Runs the CLI, reading from sys.stdin by default
